921b8f6ee7ddf7e4f6eb88d98caa335af05e4937
Cactochan <85914469+mastercodermerwin@users.noreply.github.com>
2023-03-23 22:00:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 
+export FLIP_HOME="/home/flip.3.2.1/bin"
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+sudo apt-get install default-jre:i386
+export java_32="/usr/lib/jvm/java-1.11.0-openjdk-i386/bin/java"
+sudo apt-get install libcanberra-gtk-module:i386
+sudo apt-get install libcanberra-gtk3-module:i386
